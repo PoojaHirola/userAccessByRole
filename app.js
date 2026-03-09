d224fcb95453c16a48d9190ef02acc53b1369439
PoojaHirola <poojapatel@hirolaits.com>
2026-03-09 14:14:34 +0530

    last commit

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.use(bodyParser.json())
 app.use(express.json());
 const userRoute=require('./route/route')
 app.use(userRoute)
+app.use('/hirola',(req,res)=>{
+    res.status(200).json({message:"my first deployed project"})
+})
 app.listen(3000,()=>{
     db()
     console.log("server is running 3000")
